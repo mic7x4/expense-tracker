2cb92550505c64cec02c9392996fa05ae311f745
mic7x4 <mic7x4@gmail.com>
2021-02-05 15:36:54 +0200

    adding server stuff

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import transactionRouter from './Routes/transactionsRoute.js';
+import connectDB from '../server/config/db.js';
+import bodyParser from 'body-parser';
 
 
-dotenv.config({path: 'file.env'});
 
+dotenv.config({path: 'config/file.env'});
 const app = express();
-const PORT = process.env.PORT || 8000;
-app.get('/',(req,res)=>{
-    console.log('Done');
-})
+app.use(express.json())
 
-app.listen(PORT,()=>console.log(`[!]: Server is Running in ${process.env.NODE_ENV} on Port ${PORT}`));
+// DB connection
+connectDB();
+
+const PORT = process.env.PORT || 8000;
+app.use('/api/v1/transactions',transactionRouter);
+app.listen(PORT,()=>console.log(`[!]: Server is Running in ${process.env.NODE_ENV} on Port ${PORT}`.cyan.bold));
